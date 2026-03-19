1ac772456d3e34ca06a14c36a91f521bf8831d71
Fluxo Digital Marketing <fluxodigital.cont@gmail.com>
2026-03-18 23:28:28 -0300

    Corrigindo import do Chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import Chat from './pages/Chat';
+import Chat from './components/Chat/UnifiedChat';
 import Kanban from './pages/Kanban';
 import Settings from './pages/Settings';
 import Pricing from './pages/Pricing';
